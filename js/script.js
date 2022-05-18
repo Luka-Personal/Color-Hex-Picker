37447665c12e642aed222ab8408896068a36fe60
Luka Tsinaridze <luka.tsinaridze@gmail.com>
2022-05-18 04:44:48 +0400

    mobile nav + other tweaks

--- js/script.js
+++ js/script.js
@@ -1,10 +1,66 @@
 `use strict`;
 
-console.log(`hey`);
-const span = document.querySelectorAll("[id=copy]");
-for (const item of span) {
-  item.addEventListener("click", function () {
-    navigator.clipboard.writeText(item.textContent);
-    console.log(item.textContent);
+// consts
+const hexCode = document.querySelectorAll("[id=copy]");
+const menuBtn = document.querySelectorAll(`.menu-icon`);
+const navItems = document.querySelector(`.nav-items`);
+const navItemsToggle = document.querySelector(`.nav-toggl`);
+const heroSection = document.querySelector(`.hero`);
+const shortcutSection = document.querySelector(`.shortcut`);
+const navLink = document.querySelectorAll(`.nav-link`);
+const contBlur = document.querySelectorAll(`.container`);
+const closeIcon = document.querySelector(`.helper-close`);
+const openIcon = document.querySelector(`.helper-open`);
+// ################################################
+// color hex pick on click
+for (const hx of hexCode) {
+  hx.addEventListener("click", function () {
+    navigator.clipboard.writeText(hx.textContent);
   });
 }
+// ################################################
+// FUNCTIONS
+// on click show nav menu
+const addNav = function () {
+  navItems.classList.toggle(`nav-toggl`);
+  openIcon.classList.toggle(`hide-open-icon`);
+  closeIcon.classList.toggle(`show-open-icon`);
+  for (const conts of contBlur) {
+    conts.classList.toggle(`blur-bg`);
+  }
+  document.body.classList.toggle(`scroll-disable`);
+};
+// on click remove nav menu
+const removeNav = function () {
+  navItems.classList.remove(`nav-toggl`);
+  for (const conts of contBlur) {
+    conts.classList.remove(`blur-bg`);
+  }
+  document.body.classList.remove(`scroll-disable`);
+};
+// if window size >=944 remove nav
+const removeNavWindow = function () {
+  if (this.window.innerWidth >= 944) {
+    removeNav();
+    openIcon.classList.remove(`hide-open-icon`);
+    closeIcon.classList.remove(`show-open-icon`);
+  }
+};
+// ################################################
+// open menu on click
+for (const item of menuBtn) {
+  item.addEventListener(`click`, addNav);
+}
+
+// normal click to get of the website
+for (const item of navLink) {
+  item.addEventListener(`click`, removeNav);
+}
+
+// middle click on mouse
+for (const item of navLink) {
+  item.addEventListener(`auxclick`, removeNav);
+}
+
+// remove nav on resize
+window.addEventListener("resize", removeNavWindow);
